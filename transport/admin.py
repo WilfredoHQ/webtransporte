2d2539805d0ec031f2cb5e95eac241d5736d0476
Wilfredo Huancollo <wilfredo_sj@hotmail.com>
2020-04-19 21:44:53 -0500

    modelo de la app transport regitrado en panel admin

--- transport/admin.py
+++ transport/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cooperative, City, Travel, Schedule
 
 # Register your models here.
+
+admin.site.register(Cooperative)
+admin.site.register(City)
+admin.site.register(Travel)
+admin.site.register(Schedule)
--- transport/models.py
+++ transport/models.py
@@ -4,25 +4,25 @@ from django.db import models
 
 class Cooperative(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.IntegerField
-    phone2 = models.IntegerField
+    phone = models.IntegerField()
+    phone2 = models.IntegerField()
     fax = models.CharField(max_length=50)
     mail = models.EmailField(max_length=200)
-
-
-class Travel(models.Model):
-    cooperative = models.ForeignKey(Cooperative, on_delete=models.CASCADE)
-    origin = models.ForeignKey(City, on_delete=models.CASCADE)
-    route = models.CharField(max_length=200)
-    destination = models.ForeignKey(City, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    isactive = models.BooleanField 
 
 
 class City(models.Model):
     name = models.CharField(max_length=50)
 
 
+class Travel(models.Model):
+    cooperative = models.ForeignKey(Cooperative, on_delete=models.CASCADE)
+    origin = models.ForeignKey(City, related_name='origin', on_delete=models.CASCADE)
+    route = models.CharField(max_length=200)
+    destination = models.ForeignKey(City, related_name='destination', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    isactive = models.BooleanField()
+
+
 class Schedule(models.Model):
     travel = models.ForeignKey(Travel, on_delete=models.CASCADE)
-    hout = models.DateTimeField
+    hour = models.DateTimeField()
