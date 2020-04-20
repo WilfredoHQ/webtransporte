bf48033f5d024c246fe43ede05a9f033163ac48d
Wilfredo Huancollo <wilfredo_sj@hotmail.com>
2020-04-19 21:32:47 -0500

    modelo de la app team avatar optimizado

--- team/models.py
+++ team/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 def custom_upload_to(instance, filename):
-    old_instance = Profile.objects.get(pk=instance.pk)
-    old_instance.avatar.delete()
-    return 'profiles/' + filename
+    try:
+        old_instance = Employee.objects.get(pk=instance.pk)
+        old_instance.avatar.delete()
+    finally:
+        return 'employees/' + filename
 
 # Create your models here.
 
@@ -11,8 +13,11 @@ class Employee(models.Model):
     name = models.CharField(max_length=50)
     job = models.CharField(max_length=50)
     description = models.TextField()
-    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
+    avatar = models.ImageField(upload_to=custom_upload_to)
     facebook = models.URLField(max_length=200, null=True, blank=True)
     twitter = models.URLField(max_length=200, null=True, blank=True)
     instagram = models.URLField(max_length=200, null=True, blank=True)
     mail = models.EmailField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- webtransporte/settings.py
+++ webtransporte/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'team',
+    'transport'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
